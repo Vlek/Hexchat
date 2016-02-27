15357cb0044ef929a56f665feaf652b48ce126c9
Vlek <Vlek@users.noreply.github.com>
2016-02-27 11:45:09 -0500

    Update Keepnick.py

--- HexChat/Keepnick.py
+++ HexChat/Keepnick.py
@@ -14,7 +14,7 @@ def attempt_nick_change():
 
 
 # If we log on and our nick is not our preferred nick,
-if hexchat.get_info('nick') != "Dingleberries": #hexchat.get_prefs("irc_nick1"):
+if hexchat.get_info('nick') != hexchat.get_prefs("irc_nick1"):
 
     # If there's nickserv, we're going to have a password on file for it,
     # And, if that's the case, we'll just do the usual stuff to get our nick
