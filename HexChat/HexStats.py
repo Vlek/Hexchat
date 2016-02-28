a53941e1eee016acbddf9db3096b2b3e42953276
Vlek <Vlek@users.noreply.github.com>
2016-02-28 15:22:32 -0500

    Update HexStats.py

--- HexChat/HexStats.py
+++ HexChat/HexStats.py
@@ -10,15 +10,34 @@ __module_author__ = 'Vlek'
 
 
 def stats(word, word_to_eol, userdata):
+    print( getstats() )
+    return hexchat.EAT_ALL
+
+
+def printstats(word, word_to_eol, userdata):
     context = hexchat.find_context()
+    context.command('say {}'.format( getstats() ))
+    return hexchat.EAT_ALL
+
+
+def getstats():
     chans = hexchat.get_list('channels')
     types = [i.type for i in chans]
     channels = types.count(2)
-    ops = 0
+    
+    contextlist = [i.context for i in chans if i.type == 2]
+    ops = []
+    for context in contextlist:
+        ops += [user.prefix for user in context.get_list('users') if user.nick == hexchat.get_info('nick')]
+    #ops = ops.count('@')
+
     servers = types.count(1)
     queries = types.count(3)
-    context.prnt('Stats: {} channels ({} OPs), {} servers, {} queries'.format( channels, ops,
-                                                                               servers, queries ))
-    return hexchat.EAT_ALL
+    return 'Stats: {} channels ({} OPs), {} servers, {} queries'.format( channels, ops,
+                                                                        servers, queries )
+
 
 hexchat.hook_command("stats", stats, help="/stats displays HexChat user statistics")
+hexchat.hook_command("printstats", printstats, help="/printstats Says HexChat user statistics in current context")
+
+
