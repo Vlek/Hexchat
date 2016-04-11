05624656fe351f47ce072c0196e34fa4d7869f74
Vlek <Vlek@users.noreply.github.com>
2016-04-11 13:07:55 -0400

    Create word replace.py

---
+++ HexChat/word replace.py
@@ -0,0 +1,35 @@
+import hexchat
+
+__module_name__ = 'Word Replace'
+__module_version__ = '0.0.1'
+__module_description__ = 'Replaces words with preferred ones'
+__module_author__ = 'Vlek'
+
+"""
+Just change what replacements you'd like in here.
+You could have it set to allow for a command that enables editing
+but you'd also have to save the information into the plugin's config.
+It was a lot more effort than a simple script to call Drakonis a dildo
+called for. ;)
+"""
+_replacements = {'drakonis':'dildos', 'irc':'life'}
+
+
+def check_for_replacements(word, word_to_eol, userdata):
+    global _replacements
+    result = []
+    changed = False
+    for w in word[1].split():
+        if w.lower() in _replacements.keys():
+            result.append(_replacements[w])
+            changed = True
+        else:
+            result.append(w)
+    if changed:
+        if len(word) == 2:
+            word.append('')
+        hexchat.get_context().emit_print('Channel Message', word[0], ' '.join(result), word[2])
+        return hexchat.EAT_ALL
+
+hexchat.hook_print('Channel Message', check_for_replacements, priority=hexchat.PRI_LOW)
+hexchat.hook_print('Channel Msg Hilight', check_for_replacements, priority=hexchat.PRI_LOW)
