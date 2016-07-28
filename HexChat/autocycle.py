210c26abd257c219e9e3ff7a29e88c6b327d3640
Vlek <Vlek@users.noreply.github.com>
2016-07-28 16:22:41 -0700

    Update autocycle.py

--- HexChat/autocycle.py
+++ HexChat/autocycle.py
@@ -9,7 +9,7 @@ __module_author__ = 'Vlek'
 def cycle_check(argv, arg_to_eol, c):
     context = hexchat.find_context()
     if len(context.get_list('users')) == 1:
-        context.command('/cycle')
+        context.command('cycle')
 
 
 for event in ['Part', 'Part with Reason', 'Quit']:
